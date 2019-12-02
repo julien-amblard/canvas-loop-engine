5826ddb0f1f96da8509791b15cbcb78047d3812c
Julien Amblard <amblard.julien@gmail.com>
2019-12-02 21:31:10 +0100

    update fonctions props

--- src/engine.js
+++ src/engine.js
@@ -3,39 +3,67 @@ const DEFAULT_FUNC = ({ $canvas, ctx, data }) => void 0
 class Engine {
 	constructor({ 
 		$canvas = null, 
+		autoStart = true,
+		clearOnUpdate = true,
 		data = {},
+
 		onUpdate = DEFAULT_FUNC,
 		onInit = DEFAULT_FUNC,
 		onDraw = DEFAULT_FUNC,
 		onStart = DEFAULT_FUNC,
-		onStop = DEFAULT_FUNC,
-		clearOnUpdate = true,
-		autoStart = true
+		onStop = DEFAULT_FUNC
 	} = {}) {
-		this.$canvas = $canvas instanceof HTMLElement && $canvas.tagName.toLowerCase() === "canvas" ? $canvas : null
+
+		this.$canvas = $canvas instanceof HTMLElement && $canvas.tagName.toLowerCase() === "canvas" 
+			? $canvas : null
+		
 		this.data = !!data ? cloneDeep(data) : null
-		this.onInit = typeof onInit === "function" ? onInit : null
-		this.onUpdate = typeof onUpdate === "function" ? onUpdate : null
 		this.clearOnUpdate = !!clearOnUpdate
+		
+		this.onInit = typeof onDraw === "function" 
+			? [onInit]
+			: Array.isArray(onInit)
+				? onInit.filter( fn => typeof fn === "function" )
+				: null
+		
+		this.onUpdate = typeof onUpdate === "function" 
+			? [onUpdate]
+			: Array.isArray(onUpdate)
+				? onUpdate.filter( fn => typeof fn === "function" )
+				: null
 
 		this.onDraw = typeof onDraw === "function" 
-			? [onDraw] 
+			? [onDraw]
 			: Array.isArray(onDraw)
 				? onDraw.filter( fn => typeof fn === "function" )
 				: null
 
-		this.onStart = typeof onStart === "function" ? onStart : null
-		this.onStop = typeof onStop === "function" ? onStop : null
+		this.onStart = typeof onStart === "function" 
+			? [onStart]
+			: Array.isArray(onStart)
+				? onStart.filter( fn => typeof fn === "function" )
+				: null
+
+		this.onStop = typeof onStop === "function" 
+			? [onStop]
+			: Array.isArray(onStop)
+				? onStop.filter( fn => typeof fn === "function" )
+				: null
 		
 		if( !!!this.$canvas ) console.error("canvas-engine : invalid canvas element")
 		else {
+
 			this.arg = {
 				$canvas: this.$canvas,
 				ctx: this.$canvas.getContext('2d'),
 				data: this.data
 			}
-			this.onInit( this.arg )
+
+			if( !!this.onInit && this.onInit.length ) 
+				this.onInit.forEach( fn => fn( this.arg ) )
+
 			if( autoStart === true ) this._start()
+
 		}
 	}
 	_isRunning = false
@@ -47,7 +75,8 @@ class Engine {
 		requestAnimationFrame( this._run.bind(this) )
 	}
 	_update () {
-		if( !!this.onUpdate ) this.onUpdate( this.arg )
+		if( !!this.onUpdate && this.onUpdate.length ) 
+			this.onUpdate.forEach( fn => fn( this.arg ) )
 	}
 	_draw () {
 		if( !!this.clearOnUpdate ) this._clearCanvas()
@@ -55,11 +84,15 @@ class Engine {
 			this.onDraw.forEach( fn => fn( this.arg ) )
 	}
 	_stop () {
-		if( !!this.onStop ) this.onStop( this.arg )
+		if( !!this.onStop && this.onStop.length ) 
+			this.onStop.forEach( fn => fn( this.arg ) )
+
 		this._isRunning = false
 	}
 	_start () {
-		if( !!this.onStart ) this.onStart( this.arg )
+		if( !!this.onStart && this.onStart.length ) 
+			this.onStart.forEach( fn => fn( this.arg ) )
+
 		this._isRunning = true
 		this._run()
 	}
